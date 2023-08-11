c8bacf51e5392be3ba967314ac8f98471d2d922b
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-08-11 13:16:22 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "f352f66e-c158-4fed-9e8c-be547949a19c",
+    "home_feed": [
+        {
+            "id": "0d5bbddb-cf9c-4da9-9be4-db637d5e52f8",
+            "artist_name": "he",
+            "home_carousel": [
+                {
+                    "id": "16f95733-f9b9-4121-9060-4d8deccfbf91",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "acb9532b-502a-40b4-94b7-d77d817324d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "58cbb41d-5f10-4c86-bad4-26e9e71439cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png"
+                },
+                {
+                    "id": "42793c09-09cd-46d7-bb81-78d114b1c909",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/130.png"
+                },
+                {
+                    "id": "0951960f-d5a0-4c18-820e-8f04760ecec7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                }
+            ],
+            "art_caption": "modern",
+            "art_description": "Country lawyer care support impact agent. Close back serious red eat Mr industry."
+        },
+        {
+            "id": "38f0a58a-f117-426b-9fc8-50b62c74a8d6",
+            "artist_name": "industry",
+            "home_carousel": [
+                {
+                    "id": "950d18cd-d6a4-4547-a84b-698bd9a2382b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "da3684bd-d6fe-4239-976b-141dd0b3552a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "dd7c3ffc-9ae7-4a07-9b3f-fcb5cdb4c329",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                },
+                {
+                    "id": "a8bb6c2e-f207-4115-bfcc-e42bfc85c449",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                },
+                {
+                    "id": "944c8864-f3cc-4398-98b4-209484ae4599",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/12.png"
+                }
+            ],
+            "art_caption": "by",
+            "art_description": "Explain prove her under authority moment ever. Produce lawyer team successful meet other woman."
+        },
+        {
+            "id": "1acf523c-d81c-4b0e-90c7-dd79c3899bc3",
+            "artist_name": "land",
+            "home_carousel": [
+                {
+                    "id": "5540fce8-33a0-472f-8464-8fd7bf257c23",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/161.png"
+                },
+                {
+                    "id": "fd84dafd-0c03-4906-9a06-342d8b9c8860",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "bf34c380-9a19-4a2e-b263-85e1a7f90dad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+                },
+                {
+                    "id": "e667fbab-105b-41ae-acfb-60bcedf220f7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/248.png"
+                },
+                {
+                    "id": "f17f7fb0-8e16-4c1e-aea1-33fef582f395",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+                }
+            ],
+            "art_caption": "arrive",
+            "art_description": "Everyone human trip job show should wait."
+        },
+        {
+            "id": "ef3f5743-90da-45e8-98a5-a2b0e8f3a558",
+            "artist_name": "wall",
+            "home_carousel": [
+                {
+                    "id": "422a6259-ea0d-4ac8-bd17-f5244d507ee8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "7ed1f748-f888-448f-b460-94e847aaab84",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "0c442534-18c1-4435-9cdf-2bf67d43edc3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "cccb3931-7d05-4f18-9e7d-845c042114cc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                },
+                {
+                    "id": "8530abe0-165f-400c-ac35-bb4874c247c9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/248.png"
+                }
+            ],
+            "art_caption": "however",
+            "art_description": "Member red national third relate other development. Country teacher that road anyone. Strategy describe theory happy explain floor."
+        },
+        {
+            "id": "cb8a5f1e-36fa-43eb-a0e0-2e690ae12fc4",
+            "artist_name": "phone",
+            "home_carousel": [
+                {
+                    "id": "9c2cacbe-e247-4d64-a34d-3d229905f885",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "0acc9510-6502-4daf-95eb-2039d560da97",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "36342fda-1fea-41bf-a24d-aab112e199ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/146.png"
+                },
+                {
+                    "id": "12391dc2-6d98-4876-b350-8071be5b1d28",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "17b3e23f-0a2c-459c-a07b-c13b4f6b4078",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                }
+            ],
+            "art_caption": "world",
+            "art_description": "Either that necessary. Reflect result throw partner each economy."
+        }
+    ]
+}
